0f0661e6247b042a16c5e1fa7c24a22824e827b6
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-07-18 17:27:32 +0200

    no message

--- SymconSzenenUltimateV2/module.php
+++ SymconSzenenUltimateV2/module.php
@@ -120,7 +120,7 @@
                         $newPos = $this->getHighestPosition() + 1;
                         $newInt = $this->checkInteger($scene->name, false, $this->InstanceID, $newPos, -1);
                         $this->addSetValue($newInt);
-                        $this->setProfile($newInt, $this->prefix . ".SceneOptions");
+                        $this->addProfile($newInt, $this->prefix . ".SceneOptions");
 
                     }
 
