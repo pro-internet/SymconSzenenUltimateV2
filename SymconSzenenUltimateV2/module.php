b16edd66d2eac335557990f389d777527f682f8a
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-07-18 16:21:11 +0200

    no message

--- SymconSzenenUltimateV2/module.php
+++ SymconSzenenUltimateV2/module.php
@@ -57,7 +57,7 @@
         public function CheckProfiles () {
 
             //checkVariableProfile ($name, $type, $min = 0, $max = 100, $steps = 1, $associations = null) {
-            $this->checkVariableProfile($this->prefix . ".Options", $this->varTypeByName("int"), 0, 2, 1, array("Zeige Targets" => 0, "Verstecke Targets" => 1));
+            $this->checkVariableProfile($this->prefix . ".Options", $this->varTypeByName("int"), 0, 1, 1, array("Zeige Targets" => 0, "Verstecke Targets" => 1));
 
         }
     
