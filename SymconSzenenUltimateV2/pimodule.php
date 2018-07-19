9cb86b81d3de5c0972124a1c46c09f58244c6631
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-07-19 09:32:59 +0200

    no message

--- SymconSzenenUltimateV2/pimodule.php
+++ SymconSzenenUltimateV2/pimodule.php
@@ -442,13 +442,16 @@ abstract class PISymconModule extends IPSModule {
                         if ($child == $afterThisElement) {
 
                             $elementFound = true;
+                            $subElementFound = false;
                             foreach ($own['ChildrenIDs'] as $subChild) {
 
                                 $oo = IPS_GetObject($subChild);
                                 if ($subChild == $child) {
-                                    break;
+                                    $subElementFound = true;
                                 } else {
-                                    $this->setPosition($child, $oo['ObjectPosition'] - 1);
+                                    if (!$subElementFound) {
+                                        $this->setPosition($child, $oo['ObjectPosition'] - 1);
+                                    }
                                 }
 
                             }
