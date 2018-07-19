71d6b62ec5638b533a1294ecc132c94b9bf18d8b
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-07-19 08:46:15 +0200

    - Icon hinzufügen zu Szenen

--- SymconSzenenUltimateV2/module.php
+++ SymconSzenenUltimateV2/module.php
@@ -118,6 +118,7 @@
                         $newPos = $this->getHighestPosition() + 1;
                         $newInt = $this->checkInteger($scene->name, false, $this->InstanceID, $newPos, -1);
                         $this->addSetValue($newInt);
+                        $this->addIcon($newInt, "Rocket");
                         $this->addProfile($newInt, $this->prefix . ".SceneOptions");
 
                     }
