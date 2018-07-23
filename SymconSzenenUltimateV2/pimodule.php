c125f0a317452b3639c5755e2169e156a4a67858
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-07-23 09:56:06 +0200

    no message

--- SymconSzenenUltimateV2/pimodule.php
+++ SymconSzenenUltimateV2/pimodule.php
@@ -1195,7 +1195,7 @@ abstract class PISymconModule extends IPSModule {
 
             //$name, $type, $min = 0, $max = 100, $steps = 1, $associations = null
 
-            $newAssocs = null;
+            $newAssocs = null; 
             $blockIt = array();
 
             if ($association != null) {
