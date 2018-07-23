e92c0762313a4810bc84ec43e4a4e89291a560ca
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-07-23 09:05:59 +0200

    no message

--- SymconSzenenUltimateV2/module.php
+++ SymconSzenenUltimateV2/module.php
@@ -704,6 +704,8 @@
                     $this->changeAssociations($this->prefix . ".Options" . $this->InstanceID, array("Modul vergrößern" => "Modul verkleinern"));
                     $this->addProfile($this->searchObjectByName("Optionen"), $this->prefix . ".Options" . $this->InstanceID);
 
+                    return;
+
                 }
 
             }
