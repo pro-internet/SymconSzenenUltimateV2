16ed34831a7309dc956964881954d2e3eb712077
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-07-27 16:36:45 +0200

    no message

--- SymconSzenenUltimateV2/module.php
+++ SymconSzenenUltimateV2/module.php
@@ -63,6 +63,7 @@
 
             $sensorSet = false;
             $sensorID = $this->ReadPropertyInteger("Sensor");
+            $prnt = IPS_GetParent($this->InstanceID);
 
             if ($sensorID != null) {
 
