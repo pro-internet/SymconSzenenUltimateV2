e61a0acd48b32225eadb49dde8a6dd560eeafbaa
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-08-07 18:05:01 +0200

    no message

--- SymconSzenenV2/module.php
+++ SymconSzenenV2/module.php
@@ -296,7 +296,7 @@
         public function RegisterProperties () {
     
             $this->RegisterPropertyBoolean("ModeDaySet", true);
-            $this->RegisterPropertyString("Names", "");
+            $this->RegisterPropertyString("Names", "[{\"ID\":0,\"Name\":\"Offen\"},{\"ID\":0,\"Name\":\"Ausblick\"},{\"ID\":0,\"Name\":\"Beschattung\"},{\"ID\":0,\"Name\":\"Geschlossen\"}]");
             $this->RegisterPropertyBoolean("ModeTime", false);
             $this->RegisterPropertyBoolean("Loop", false);
             $this->RegisterPropertyInteger("Sensor", null);
