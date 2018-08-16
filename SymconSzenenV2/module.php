708d97f0c0f41ab72066514c42596cced28da704
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2018-08-16 17:02:46 +0200

    no message

--- SymconSzenenV2/module.php
+++ SymconSzenenV2/module.php
@@ -1647,7 +1647,7 @@
 
                         } else {
 
-                            echo "Hash " . md5(json_encode($states) . " in List";
+                            echo "Hash " . md5(json_encode($states)) . " in List";
 
                             foreach ($this->getSceneHashList() as $kkey => $kval) {
 
